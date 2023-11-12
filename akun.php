a503893679da3b0e4d2cdd1eceb8362a56a4f034
FaisAkbar <akbarfais06@gmail.com>
2023-11-13 00:00:44 +0700

    plugins

--- akun.php
+++ akun.php
@@ -19,7 +19,7 @@ if (!isset($_SESSION['status'])) {
     <link rel="stylesheet" href="assets/css/vendor/iconfont.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/plugins/plugins.css">
+    <link rel="stylesheet" href="assets/css/plugins/pluginsfix.css">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if (isset($_SESSION['status']) and isset($_SESSION['privilege'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/vendor/iconfont.min.css">
-    <link rel="stylesheet" href="assets/css/plugins/plugins.css">
+    <link rel="stylesheet" href="assets/css/plugins/pluginsfix.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <style>
         .btn {
